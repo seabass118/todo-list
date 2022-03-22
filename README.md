9f71066816b104b502419daa4cb5b1e01f4bfdf4
seabass118 <seb.blackley@protonmail.com>
2022-03-22 10:57:21 +0000

    commit #3 (removed localStorage)

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # todo-list
 Simple Todo List created using vanilla JavaScript.
 
-`Saves to localStorage` <br>
 `Option to delete list items` <br>
 `Simple UI`
--- Scripts/main.js
+++ Scripts/main.js
@@ -4,42 +4,6 @@ const list_element  = document.querySelector('.app-list'); //List element
 let list = []; //Init list
 let counter = 0; //Init counter
 
-const loadList = (list) => { //Takes in the list from localStorage
-
-    let placehol = document.querySelector(".app-placeholder");
-
-    if(list.length !== 0) { //If the list is not empty
-
-        counter = list.length; //Set counter to the length of the list
-
-        placehol.style.display = "none"; //Hide the placeholder text
-
-        for(let i = 0; i < list.length; i++) { //Loop through the list
-
-            list_element.innerHTML += `
-                <div class="app-list-item item-${list.length}">
-                    <div class="app-list-item-text">
-                        ${list[i]}
-                    </div>
-                    <div class="app-list-item-remove" onclick="removeItem(${list.length})">
-                        x
-                    </div>
-                </div>
-            `; //Generate a new element for each item in the list
-
-        }
-
-    } else { //If the list is empty
-
-        placehol.style.display = "flex"; //Show the placeholder
-
-    }
-    
-}
-
-list = JSON.parse(localStorage.getItem("list")); //Gets the list from localStorage
-loadList(list); //Calls the loadList function passing in the localStorage list
-
 form_element.addEventListener('submit', () => { //On submit
 
     let placehol = document.querySelector('.app-placeholder'); //
@@ -88,7 +52,6 @@ const onSubmit = () => {
 
     getInput(); //Calls getInput()
 
-    localStorage.setItem("list", JSON.stringify(list)); //Adds the current list to local storage
 
     return false; //Stops page from refreshing on form submit
 
@@ -102,7 +65,6 @@ const removeItem = (itemNumber) => { //Takes in the item number (called onclick)
     counter--; //Decrements counter
 
     list.splice(itemNumber - 1); //Removes item from list
-    localStorage.setItem("list", JSON.stringify(list)); //Updates the list
 
     selectedClass = ".item-" + itemNumber; //Specifys the classname
     selectedElement = document.querySelector(selectedClass); //Finds the element
